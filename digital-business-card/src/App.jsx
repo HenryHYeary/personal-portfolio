434bbec9a45cb291ab073789c12032d3210d9d48
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2024-04-06 17:25:53 -0500

    Added description and socials bar.

--- digital-business-card/src/App.jsx
+++ digital-business-card/src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 function App() {
 
   return (
+  <>
     <div id="main-component">
-       <img src="../public/profile-photo.jpg" alt="Profile Photo" id="profile-photo"/>
+       <img src="./profile-photo.jpg" alt="Profile Photo" id="profile-photo"/>
        <h1 id="name-header">Henry Yeary</h1>
-       <h3 id='job-header'>Frontend Developer</h3>
+       <h3 id='job-header'>Fullstack Developer</h3>
        <h5 id="website">henryyeary.website</h5>
        <div id='block-container'>
           <div className="block" id="email-block">
@@ -16,7 +17,27 @@ function App() {
             <h2><a className='block-text' id="linkedin" href='https://www.linkedin.com/in/henry-yeary-9b0901182/'>LinkedIn</a></h2>
           </div>
        </div>
+       <div id="description">
+          <h2 className='sub-header'>About</h2>
+          <p className='paragraph'>I am a fullstack developer with over a thousand hours spent working in frontend and backend JS. I love learning
+          new design patterns, and I have a particular interest in making visually engaging content.</p>
+          <h2 className='sub-header'>Interests</h2>
+          <p className='paragraph'>Video game aficinado. Avid reader. Enjoyer of all genres of music and film. Coffee connosieur.</p>
+       </div>
     </div>
+    <div id="socials-bar">
+      <a href="https://twitter.com/Henmo501">
+        <img src='./twitter-icon.png' alt='twitter-icon'className="icon" id='twitter-icon'/>
+      </a>
+      <a href="https://github.com/HenryHYeary">
+        <img src="./github-icon.png" alt="github-icon" className="icon" id='github-icon' />
+      </a>
+      <a href="https://www.facebook.com/hyeary/">
+        <img src="./facebook-icon.png" alt="facebook-icon" className="icon" id='facebook-icon' />
+      </a>
+    </div>
+  </>
+    
   )
 }
 
