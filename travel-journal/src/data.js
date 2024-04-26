3c6f2c0625b0af684af826c6dae71d548d56068b
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2024-04-25 22:16:29 -0500

    Added some finishing touches.

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -1,20 +1,29 @@
 export default [
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    title: "Taj Mahal",
+    location: "India",
+    googleMapsUrl: "https://www.google.com/maps/place/Taj+Mahal/@27.1752548,78.0392796,16.09z/data=!4m6!3m5!1s0x39747121d702ff6d:0xdd2ae4803f767dde!8m2!3d27.1751448!4d78.0421422!16zL20vMGw4Y2I?entry=ttu",
+    startDate: "January 12, 2019",
+    endDate: "January 28, 2019",
+    description: "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor, Shah Jahan (reigned from 1628 to 1658), to house the tomb of his favourite wife, Mumtaz Mahal.",
+    imageUrl: "https://source.unsplash.com/people-walking-near-white-concrete-building-during-daytime-p4-d9whI0Ts"
   },
   {
     title: "Loch Ness",
     location: "Scotland",
     googleMapsUrl: "https://www.google.com/maps/place/Loch+Ness/@57.2639609,-4.4807383,11z/data=!4m6!3m5!1s0x488f16c34da23729:0xd4d749cbf4fe912f!8m2!3d57.3228575!4d-4.4243818!16zL20vMDRwMTc?entry=ttu",
-    startDate: "15 June 2018",
-    endDate: "23 June 2018",
-    description: "Loch Ness, lake, lying in the Highland council area, Scotland. With a depth of 788 feet (240 metres) and a length of about 23 miles (36 km), Loch Ness has the largest volume of fresh water in Great Britain. It lies in the Glen Mor—or Great Glen, which bisects the Highlands—and forms part of the system of waterways across Scotland that civil engineer Thomas Telford linked by means of the Caledonian Canal (opened 1822).",
+    startDate: "June 15, 2018",
+    endDate: "June 23, 2018",
+    description: "Loch Ness, lake lies in the Highland council area of Scotland. With a depth of 788 feet (240 metres) and a length of about 23 miles (36 km), Loch Ness has the largest volume of fresh water in Great Britain.",
     imageUrl: "https://source.unsplash.com/gray-structure-near-sea-nIzIbrytpfA"
+  },
+  {
+    title: "Cathedral of Santa Maria del Fiore",
+    location: "Italy",
+    googleMapsUrl: "https://www.google.com/maps/place/Cathedral+of+Santa+Maria+del+Fiore/@43.773145,11.2559602,15z/data=!4m6!3m5!1s0x132a5403bfe22ff5:0x5591438487aaf1f5!8m2!3d43.773145!4d11.2559602!16zL20vMDFnODJm?entry=ttu",
+    startDate: "May 16, 2015",
+    endDate: "May 20, 2015",
+    description: "The Cathedral of Santa Maria del Fiore was completed in 1434 and is the most important landmark in Florence, as well as being the fourth largest church in the world.",
+    imageUrl: "https://source.unsplash.com/a-large-building-with-a-dome-and-a-clock-on-it-StmeheQr3Sc"
   }
 ]
